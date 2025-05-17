9be18b5fc7521e2d17746eac8f91fde022a65451
Pgaikwad203 <priyanshugaikwad30@gmail.com>
2025-05-17 14:38:06 +0530

    fatch data and display on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
-import { useEffect, useState } from 'react';
-import LoginForm from './LoginForm';
-import RegistrationForm from './RegistrationFrom';
+import {useEffect, useState} from "react";
+import LoginForm from "./LoginForm";
+import RegistrationForm from "./RegistrationFrom";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+
 function App() {
   const [childdata, setChilddata] = useState("");
   const [childdata2, setChilddata2] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
       const data = await response.json();
+      setUsers(data);
       console.log(data);
     }
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <div className="app-container">
-      <p>Login form data : <br />{childdata}</p>
-      <p>registration form data : <br />{childdata2}</p>
+      <h1>Login form data : </h1>
+      <h2>{childdata}</h2>
+      <h1>Registration form data : </h1>
+      <h2>{childdata2}</h2>
       <LoginForm SendData={setChilddata} />
+      <br />
       <RegistrationForm SendData2={setChilddata2} />
-
       <div>
-
+        <h1>Users List</h1>
+        {users.map((user: User) => (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <p>{user.phone}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
